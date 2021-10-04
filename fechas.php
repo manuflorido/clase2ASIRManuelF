f5a709e01187f2e1dbece05c85c23f35f069ed8e
manuflorido <floga212@gmail.com>
2021-10-04 09:33:34 +0200

    Cambios en index y creado el primer PHP

---
+++ fechas.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio Fechas</title>
+</head>
+<body>
+    <h2>Ejercicio de tratamiento de fechas</h2>
+
+    <p>Vamos a aprender a tratar fechas en php y funcionalidades</p>
+</body>
+</html>
