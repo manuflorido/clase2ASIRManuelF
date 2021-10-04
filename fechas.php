67e0a0d2f5021a522c67cb8a8540b38b0fc5e7ce
manuflorido <floga212@gmail.com>
2021-10-04 10:21:21 +0200

    Pruebas IF y ELSEIF, cambios en el nombre de la página principal

--- fechas.php
+++ fechas.php
@@ -15,7 +15,30 @@
 
 <?php
     echo "Vamos a trabajar con fechas" . "<br>";
-    echo "Hoy es: " . date("l");
+    echo "Hoy es: " . date("l") . "<br>";
+    echo "Dime la fecha actual: " . date("Y-m-d") . "<br>". "<br>". "<br>";
+
+$hora = date("H");
+ echo "Ejercicio ejemplo IF" . "<br>";
+ echo "Son las: " . date("h:i:s") . "<br>";
+    if($hora<15){
+        echo "Estoy en el aula 5 churri" . "<br>" . "<br>". "<br>";
+    }else {
+        echo "Estoy ya en casa brrrr" . "<br>". "<br>". "<br>";
+    }
+
+$segundos = date("s");
+    echo "Ejercicio ejemplo ELSEIF" . "<br>";
+    echo "$segundos" . "<br>";
+      if($segundos<=10){
+         echo "Primeros 10 segundos" . "<br>";
+      }elseif($segundos>=50) {
+         echo "Últimos 10 segundos" . "<br>";
+      }else {
+         echo "Segundos intermedios" . "<br>";
+      }
+
+    
 ?>
 
 </body>
