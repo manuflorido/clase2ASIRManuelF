2f6b4571e6077313ac04ccce59018a16e07f50a6
manuflorido <floga212@gmail.com>
2021-10-04 09:48:54 +0200

    Pruebas fechas PHP

--- fechas.php
+++ fechas.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio Fechas</title>
+    <title>Ejercicio Fechas <?php echo date("d/n/Y");?></title>
 </head>
 <body>
+
     <h2>Ejercicio de tratamiento de fechas</h2>
 
     <p>Vamos a aprender a tratar fechas en php y funcionalidades</p>
+    <br>
+
+<?php
+    echo "Vamos a trabajar con fechas" . "<br>";
+    echo "Hoy es: " . date("l");
+?>
+
 </body>
 </html>
