a232d527a582dd55efbaa74a3b972eeea351ce13
manuflorido <floga212@gmail.com>
2021-11-08 10:23:16 +0100

    Hechos ejercicios ARRAY

--- Ejercicios/Clase/array.php
+++ Ejercicios/Clase/array.php
@@ -1,5 +1,125 @@
-<?php 
+<?php
+//Bucles anidados independientes
+print "<b>Bucle independiente</b><br>";
+for ($inicio=0; $inicio < 5; $inicio++) { 
+        print "<b>Vamos por la iteración inicio: " . $inicio . "</b><br>"; //Columnas
+    for ($inicio2=0; $inicio2 < 5; $inicio2++) { 
+        
+        print "Vamos por la iteración inicio2: " . $inicio2 . "<br>"; //Filas
+    }
+}
 
+
+//Bucles anidados dependientes
+print "<br><b>Bucle dependiente</b><br>";
+for ($varExterior=0; $varExterior < 5; $varExterior++) { 
+    print "<b>Variable exterior: " . $varExterior . "</b><br>";
+
+    for ($varInterior=0; $varInterior < $varExterior; $varInterior++) { 
+        print "Variable interior: " . $varInterior . "<br>";
+    }
+}
+
+
+//ARRAYS
+print "<br><b>Arrays 'Estándar'</b><br>";
+
+#Forma 1
+$numerosP = array(2, 4, 6, 8, 10);
+print_r($numerosP);
+print "Muestro posición 3: " . $numerosP[3] . "<br>";
+#Forma 2
+$numerosI = [1, 3, 5, 7, 9];
+print_r($numerosI);
+print "Muestro posición 3: " . $numerosI[3] . "<br>";
+
+//Mostrar Array bonito
+print "<br><b>Mostrar Array Bonito</b><br>";
+$cuentaNumerosP=count($numerosP);
+echo "Valor de la cuenta de Array NumerosP: " . $cuentaNumerosP . "<br>";
+for ($i=0; $i < $cuentaNumerosP; $i++) { 
+    print "Muestro posición " . $i . ": " . $numerosP[$i] . "<br>";
+}
+
+
+//ARRAYS ASOCIATIVOS
+print "<br><b>Arrays 'Asociativos'</b><br>";
+
+$potencias2 = [1=>2, 2=>4, 3=>8];
+$capitales = array("Madrid" => "Madrid",
+ "Andalucia" => "Sevilla",
+  "Aragón" => "Zaragoza",
+   "Baleares" => "Palma de Mallorca");
+print_r ($capitales);
+echo "<br>";
+
+print_r ($potencias2);
+echo "<br>";
+
+//Añadir elementos a un array
+print "<br><b>Añadir elementos en un Array</b><br>";
+$capitales["Cataluña"] = "Barcelona";
+print_r ($capitales);
+echo "<br>";
+
+$capitales["Andalucia"]="Almeria";
+print_r ($capitales);
+echo "<br>";
+
+print "<br><b>Contar letras de un Array</b><br>";
+$cuentaLetras = strlen($capitales["Andalucia"]);
+echo $capitales["Andalucia"] . ": " . $cuentaLetras . "<br>";
+
+print "<br><b>Mostrar ARRAY notas</b><br>";
+$notasAsignatura=["LMI"=>0,
+ "EIE"=>4,
+  "PAR"=>8,
+   "ISO"=>5,
+    "FH"=>7,
+     "BD"=>3];
+
+print_r($notasAsignatura);
+echo "<br>";
+
+
+//Unión de ARRAYS
+print "<br><b>Unión de ARRAYS</b><br>";
+$coches1 = ["Seat", "Renault", "Dacia"];
+$coches2 = ["BMW", "AUDI"];
+print_r($coches1);
+echo "<br>";
+print_r($coches2);
+echo "<br>";
+$union1con2=$coches1+$coches2;
+print_r($union1con2);
+echo "<br>";
+$union2con1=$coches2+$coches1;
+print_r($union2con1);
+echo "<br>";
+
+//Borrar elemento dentro de un ARRAY
+print "<br><b>Eliminar elemento ARRAYS</b><br>";
+$coches3 = ["Seat", "Renault", "Dacia", "BMW", "AUDI"];
+print_r($coches3);
+echo "<br>";
+unset($coches3[1]);
+print_r($coches3);
+echo "<br>";
+print $coches3[]="Ferrari";
+print_r($coches3);
+echo "<br>";
+
+//Copiar matrices
+print "<br><b>Copiar matrices</b><br>";
+$copiaCapitales = $capitales;
+$copiaCapitales["Andalucia"] = "Córdoba";
+print_r($copiaCapitales);
+echo "<br>";
+
+
+
+
+/*
     $numeros = array(8,3,7,1);
     $suspensos = array("Migue", "Kerry");
     $coches = array("Ferrari", "Seat", "BMW", "Opel", "Audi");
@@ -41,4 +161,5 @@
                         "estaAprobado" => false
                     );
     echo $alumno["Nombre"] . " " . $alumno["Apellidos"];
+    */
 ?>
